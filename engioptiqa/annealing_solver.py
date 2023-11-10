f2476c3546b376b0aa17c92cef9fab3e6f10eee5
Key, Fabian <fabian.key@tuwien.ac.at>
2023-11-10 09:08:28 +0100

    Restructure setup of annealing solvers

--- engioptiqa/annealing_solver.py
+++ engioptiqa/annealing_solver.py
@@ -5,7 +5,7 @@ from amplify.client import FixstarsClient
 from amplify.client.ocean import DWaveSamplerClient, LeapHybridSamplerClient
 
 from dwave.cloud import Client
-from dwave.samplers import SimulatedAnnealingSampler
+from dwave.samplers import SimulatedAnnealingSampler, SteepestDescentSolver
 from dwave.system import DWaveSampler, EmbeddingComposite, LeapHybridSampler
 
 
@@ -14,8 +14,7 @@ class AnnealingSolver(ABC):
     def __init__(self, token_file, proxy=None):
        self.proxy = proxy
        self.token = open(token_file,"r").read().replace('\n', '')
-       self.client = self.setup_client()
-       self.solver = self.setup_solver()
+       self.setup_client()
 
     @abstractmethod
     def setup_client(self):
@@ -38,19 +37,18 @@ class AnnealingSolverAmplify(AnnealingSolver):
     def setup_client(self):
 
         if self.client_type == 'fixstars':
-            client = FixstarsClient(token=self.token)
+            self.client = FixstarsClient(token=self.token)
         elif self.client_type == 'dwave':
-            client = DWaveSamplerClient(token=self.token)
+            self.client = DWaveSamplerClient(token=self.token)
         elif self.client_type == 'dwave_hybrid':
-            client = LeapHybridSamplerClient(token=self.token)
+            self.client = LeapHybridSamplerClient(token=self.token)
         else:
             raise Exception('Unknown client type', self.client_type)
         if self.proxy:
-            client.proxy = self.proxy
+            self.client.proxy = self.proxy
         if self.client_type in ['dwave','dwave_hybrid']:
-            print('Available solvers:', client.solver_names)
+            print('Available solvers:', self.client.solver_names)
 
-        return client
     
     def setup_solver(self):
 
@@ -59,18 +57,16 @@ class AnnealingSolverAmplify(AnnealingSolver):
             print('Setting default timeout (ms): 800')
             self.client.parameters.timeout = 800
         elif self.client_type == 'dwave': # DWave
-            print('Choosing default solver: Advantage_system6.2')
-            self.client.solver = "Advantage_system6.2"
+            print('Choosing default solver: Advantage_system4.1')
+            self.client.solver = "Advantage_system4.1"
             print('Setting default num_reads: 200')
             self.client.parameters.num_reads = 200  # Number of executions
         elif self.client_type == 'dwave_hybrid': # DWave Hybrid
             print('Choosing default solver: hybrid_binary_quadratic_model_version2')
             self.client.solver = 'hybrid_binary_quadratic_model_version2'
 
-        solver = Solver(self.client)
+        self.solver = Solver(self.client)
         print("Created solver")
-
-        return solver
 
 
     def solve_qubo_problem(self, problem):
@@ -80,8 +76,8 @@ class AnnealingSolverAmplify(AnnealingSolver):
 
 class AnnealingSolverDWave(AnnealingSolver):
 
-    def __init__(self, solver_type, token_file, proxy=None):
-        self.solver_type = solver_type
+    def __init__(self, token_file, proxy=None):
+        self.client_type = 'dwave'
         super().__init__(token_file, proxy)
 
     def setup_client(self):
@@ -94,32 +90,38 @@ class AnnealingSolverDWave(AnnealingSolver):
         for solver in self.client.get_solvers(name__regex='hybrid_binary.*', online=True):
             print("\t", solver)
 
-    def setup_solver(self):
-
-        if self.solver_type == 'quantum_annealing':
-            if self.proxy:
-                solver = EmbeddingComposite(DWaveSampler(token=self.token, proxy=self.proxy))
+    def setup_solver(self, solver_type=None, solver_name=None):
+        
+        if solver_name:
+            self.solver_name = solver_name
+            solver = self.client.get_solver(name=self.solver_name)
+            if solver.qpu:
+                self.solver_type = 'qpu'
+                self.solver = EmbeddingComposite(DWaveSampler(token=self.token, proxy=self.proxy, solver=dict(name=self.solver_name)))
             else:
-                solver = EmbeddingComposite(DWaveSampler(token=self.token))
-            print("Created solver as EmbeddingComposite(DWaveSampler)")   
-            print("Solver: ", solver.child.properties["chip_id"]) 
+                self.solver_type = 'hybrid'
+                self.solver = LeapHybridSampler(token=self.token, proxy=self.proxy)
 
-        elif self.solver_type == 'hybrid':
-            if self.proxy:
-                solver = LeapHybridSampler(token=self.token, proxy=self.proxy) 
+        
+        elif solver_type:
+            self.solver_type = solver_type
+            if self.solver_type == 'qpu':
+                self.solver = EmbeddingComposite(DWaveSampler(token=self.token, proxy=self.proxy))
+                self.solver_name = self.solver.child.properties["chip_id"]
+
+            elif self.solver_type == 'hybrid':
+                self.solver = LeapHybridSampler(token=self.token, proxy=self.proxy)
+                self.solver_name =  self.solver.properties["category"] + ' ' + self.solver.properties["version"]
+
+            elif self.solver_type == 'simulated_annealing':
+                self.solver = SimulatedAnnealingSampler() 
+                self.solver_name = 'simulated annealing'
             else:
-                solver = LeapHybridSampler(token=self.token) 
-            print("Created solver as LeapHybridSampler")
-            print("Version: ", solver.child.properties["version"])  
-
-        elif self.solver_type == 'simulated_annealing':
-            solver = SimulatedAnnealingSampler() 
-            print("Created solver as SimulatedAnnealingSampler")
-
+                raise Exception('Unknown solver type', self.solver_type)
         else:
-            raise Exception('Unknown solver type', self.solver_type)
-
-        return solver
+            raise Exception('Either a solver type or a specific solver name must be specified.')
+        
+        print("Use", self.solver_type, "solver:", self.solver_name)
 
     def solve_qubo_problem(self, problem, **kwargs):
 
@@ -128,8 +130,24 @@ class AnnealingSolverDWave(AnnealingSolver):
             **kwargs
         )
         print('Number of solutions:', len(problem.results_indices))
-        problem.results =  problem.results_indices.relabel_variables(
-            problem.label_mapping_inverse,
-            inplace=False)
-        #problem.binary_quadratic_model.relabel_variables(problem.label_mapping_inverse,inplace=True)
+        if hasattr(problem, 'label_mapping_inverse'):
+            problem.results =  problem.results_indices.relabel_variables(
+                problem.label_mapping_inverse,
+                inplace=False)
+            #problem.binary_quadratic_model.relabel_variables(problem.label_mapping_inverse,inplace=True)
+        else:
+            problem.results = problem.results_indices 
 
+    def perform_local_search(self, problem):
+
+        if hasattr(problem, 'results_indices'):
+            solver_greedy = SteepestDescentSolver()
+            sampleset_pp = solver_greedy.sample(
+                problem.binary_quadratic_model_indices,
+                initial_states=problem.results_indices
+                ) 
+            problem.results_pp = sampleset_pp.relabel_variables(
+                problem.label_mapping_inverse,
+                inplace=False)
+        else:
+            raise Exception('Trying to perform local search altough no results exist yet.')
