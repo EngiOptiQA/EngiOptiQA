6b48ecf61f4c023643b75355cd2bc1a4f79f9ff1
Key, Fabian <fabian.key@tuwien.ac.at>
2026-04-09 14:14:58 +0200

    Introduce QAOA solver with PennyLane

--- engioptiqa/__init__.py
+++ engioptiqa/__init__.py
@@ -1,10 +1,12 @@
 from .annealing_solvers import *
 from .problems import *
+from .vqa_solvers import *
 __all__ = [
     "AnnealingSolverAmplify",
     "AnnealingSolverDWave",
     "AnnealingSolverOpenJij",
     "DesignOptimizationProblemRod1D",
+    "QAOASolverPennylane",
     "Rod1D",
     "StructuralAnalysisProblemRod1D",
     "TrussStructure",
