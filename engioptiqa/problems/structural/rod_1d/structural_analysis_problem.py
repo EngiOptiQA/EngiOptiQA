8cdeadf02f0f78b628477871cfdc438bbf191dff
Damiano Baschiera <baschiera@ilsb.tuwien.ac.at>
2026-09-09 16:44:36 +0200

    Removed new line

--- engioptiqa/problems/structural/rod_1d/structural_analysis_problem.py
+++ engioptiqa/problems/structural/rod_1d/structural_analysis_problem.py
@@ -15,7 +15,7 @@ class StructuralAnalysisProblemRod1D(BaseProblemRod1D):
 
     def get_initial_number_of_problem_variables(self):
         return self.rod.n_comp * self.n_qubits_per_var
-        
+
 
     def generate_discretization(self, n_qubits_per_var, binary_representation, lower_lim=None, upper_lim=None):
         BaseProblemRod1D.initialize_discretization(self)
